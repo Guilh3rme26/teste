c31684132d43bc0c8e506e6441226a279ec3b602
Guilherme dos Reis Arruda <108768641+Guilh3rme26@users.noreply.github.com>
2022-08-05 19:13:15 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,3 +1,6 @@
 Iniciando no github
 
+
+Aqui estarei postando as atividades que foram propostas pelo 7daysofcode, ou seja, serão 7 atividades que fiz durante uma semana que me desafiaram e me fizeram entender mais como funciona o Javascript!!!
+
 :)
